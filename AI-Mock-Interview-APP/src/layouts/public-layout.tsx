a0b5fc15a9e0119cc44e80902e0964c460752c0a
spandana1245 <spandanaelitam1245@gmail.com>
2025-07-06 19:03:08 +0530

    Changes:Firebase configurations and user data saved successfully  using Handlers

--- AI-Mock-Interview-APP/src/layouts/public-layout.tsx
+++ AI-Mock-Interview-APP/src/layouts/public-layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import AuthHandler from "@/handlers/auth-handler";
 
 const PublicLayout = () => {
   return (
     <div className='w-full'>
+        <AuthHandler/>
         <Header/>
 
         <Outlet/>
